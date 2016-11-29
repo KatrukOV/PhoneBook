65234f3ea6ad3802889a26027c261200db09fbe9
KatrukOV <KatrukOV@gmail.com>
2016-11-29 14:10:32 +0200

    add Logger

--- src/main/java/com/katruk/PhoneBookApplication.java
+++ src/main/java/com/katruk/PhoneBookApplication.java
@@ -2,19 +2,11 @@ package com.katruk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-import java.util.Locale;
+
 
 @SpringBootApplication
 public class PhoneBookApplication {
-
   public static void main(String[] args) {
-//    Locale.setDefault(Locale.ENGLISH);
     SpringApplication.run(PhoneBookApplication.class, args);
   }
 }
---
+++ src/test/java/com/katruk/domain/util/ConverterTest.java
@@ -0,0 +1,35 @@
+package com.katruk.domain.util;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConverterTest {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ConverterTest.class);
+
+  @Before
+  public void setUp() throws Exception {
+
+  }
+
+  @After
+  public void tearDown() throws Exception {
+
+  }
+
+  @Test
+  public void makeUserFromDto() throws Exception {
+
+  }
+
+  @Test
+  public void makeDtoFromContact() throws Exception {
+
+  }
+
+}
