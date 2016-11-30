d3ef8f7d4a5be46a7f351a567abc1ffb32fd85ad
KatrukOV <KatrukOV@gmail.com>
2016-11-30 11:18:01 +0200

    add Logger

--- src/test/java/com/katruk/domain/util/ConverterTest.java
+++ src/test/java/com/katruk/domain/util/ConverterTest.java
@@ -1,9 +1,20 @@
 package com.katruk.domain.util;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.katruk.domain.dto.ContactDto;
+import com.katruk.domain.dto.UserDto;
+import com.katruk.domain.entity.Contact;
+import com.katruk.domain.entity.User;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,9 +23,20 @@ public class ConverterTest {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(ConverterTest.class);
 
+  @InjectMocks
+  private Converter converter;
+
+  @Mock
+  private UserDto userDto;
+
+  @Mock
+  private Contact contact;
+
+
   @Before
   public void setUp() throws Exception {
-
+//    User user = new User();
+//    UserDto userDto = new UserDto();
   }
 
   @After
@@ -25,11 +47,17 @@ public class ConverterTest {
   @Test
   public void makeUserFromDto() throws Exception {
 
+    when(converter.makeUserFromDto(userDto)).thenReturn(mock(User.class));
+
+    verify(converter, times(1)).makeUserFromDto(userDto);
   }
 
   @Test
   public void makeDtoFromContact() throws Exception {
 
+    when(converter.makeDtoFromContact(contact)).thenReturn(mock(ContactDto.class));
+
+    verify(converter, times(1)).makeDtoFromContact(contact);
   }
 
 }
