3768985c519578e1638709202f0d1fd9d636c78b
KatrukOV <KatrukOV@gmail.com>
2016-12-06 11:11:11 +0200

    add MessageServiceImplTest

---
+++ src/test/java/com/katruk/domain/service/impl/MessageServiceImplTest.java
@@ -0,0 +1,124 @@
+package com.katruk.domain.service.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.katruk.domain.service.MessageService;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.constraints.AssertFalse;
+
+@RunWith(JUnit4.class)
+public class MessageServiceImplTest {
+
+  @Rule
+  public MockitoRule rule = MockitoJUnit.rule();
+
+  @Mock
+  private HttpSession httpSession;
+//  @Spy
+
+  //  private MessageServiceImpl.Message message = new MessageServiceImpl(httpSession).new Message(
+//    MessageServiceImpl.MessageType.ERROR,"y");
+
+
+  private List<MessageServiceImpl.Message> messages;
+
+  private MessageService messageService;
+
+  @Before
+  public void setUp() throws Exception {
+    this.messageService = new MessageServiceImpl(httpSession);
+    this.messages = new ArrayList<>();
+  }
+
+  @Test
+  public void addInfoMessage_text() throws Exception {
+    //given
+    String msg = "info";
+
+    //when
+    when(this.httpSession.getAttribute(anyString())).thenReturn(messages);
+    this.messageService.addInfo(msg);
+    String result = messages.get(0).getText();
+
+    //then
+    assertEquals(msg, result);
+  }
+
+  @Test
+  public void addInfoMessage_type() throws Exception {
+    //given
+    String msg = "info";
+    MessageServiceImpl.MessageType mType = MessageServiceImpl.MessageType.INFO;
+
+    //when
+    when(this.httpSession.getAttribute(anyString())).thenReturn(messages);
+    this.messageService.addInfo(msg);
+    String result = messages.get(0).getType().toString();
+
+    //then
+    assertEquals("INFO", result);
+  }
+
+  @Test
+  public void addErrorMessage_text() throws Exception {
+    //given
+    String msg = "error";
+
+    //when
+    when(this.httpSession.getAttribute(anyString())).thenReturn(messages);
+    this.messageService.addError(msg);
+    String result = messages.get(0).getText();
+
+    //then
+    assertEquals(msg, result);
+  }
+
+  @Test
+  public void addErrorMessage_type() throws Exception {
+    //given
+    String msg = "error";
+    MessageServiceImpl.MessageType mType = MessageServiceImpl.MessageType.ERROR;
+
+    //when
+    when(this.httpSession.getAttribute(anyString())).thenReturn(messages);
+    this.messageService.addError(msg);
+    String result = messages.get(0).getType().toString();
+
+    //then
+    verify(this.httpSession,times(1)).getAttribute(anyString());
+    assertEquals("ERROR", result);
+  }
+  @Test
+  public void addErrorMessage_was_empty() throws Exception {
+    //given
+    String msg = "error";
+
+    //when
+    when(this.httpSession.getAttribute(anyString())).thenReturn(null);
+    this.messageService.addError(msg);
+
+    //then
+    verify(this.httpSession,times(1)).getAttribute(anyString());
+    assertTrue(messages.isEmpty());
+  }
+
+}
