290156f0c9da5bd279ba96b3231573a9e381b919
KatrukOV <KatrukOV@gmail.com>
2017-06-21 23:26:09 +0300

    remove @Transactional from DaoMySql

--- src/main/java/com/katruk/dao/mysql/ContactDaoMySql.java
+++ src/main/java/com/katruk/dao/mysql/ContactDaoMySql.java
@@ -6,11 +6,9 @@ import com.katruk.domain.entity.Contact;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Profile(value = "mysql")
-@Transactional
 public interface ContactDaoMySql extends JpaRepository<Contact, Long>, ContactDao {
 
 }
--- src/main/java/com/katruk/dao/mysql/PersonDaoMySql.java
+++ src/main/java/com/katruk/dao/mysql/PersonDaoMySql.java
@@ -6,11 +6,9 @@ import com.katruk.domain.entity.Person;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Profile(value = "mysql")
-@Transactional
 public interface PersonDaoMySql extends JpaRepository<Person, Long>, PersonDao {
 
 }
